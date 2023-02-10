57edd126544a94b1d0c86cbaad6a9497dfe11025
cisar2218 <cisar2218@gmail.com>
2023-02-11 00:13:42 +0100

    minor style changes

--- PlutoMessage.cs
+++ PlutoMessage.cs
@@ -7,20 +7,20 @@ namespace Plutonication
 {
     public class PlutoMessage
     {
+        public MessageCode Identifier { get; set; }
+        public String CustomData { get; set; }
         public PlutoMessage(MessageCode id, String CustomData)
         {
             Identifier = id;
             CustomData = CustomData;
         }
-
-        public MessageCode Identifier {get;set;}
-        public String CustomData {get;set;}
-
-        public Byte[] CustomDataAsBytes() {
+        public Byte[] CustomDataAsBytes()
+        {
             return System.Text.Encoding.ASCII.GetBytes(CustomData);
         }
-        public Byte[] ToByteArray() {
-            Byte[] targetArray = new Byte[CustomData.Length+1];
+        public Byte[] ToByteArray()
+        {
+            Byte[] targetArray = new Byte[CustomData.Length + 1];
             targetArray[0] = (byte)Identifier;
             Array.Copy(CustomDataAsBytes(), 0, targetArray, 1, CustomData.Length);
             return targetArray;
