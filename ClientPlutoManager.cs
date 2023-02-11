cdb0889bcda11b2ba3ff000de93528423a1f665f
cisar2218 <cisar2218@gmail.com>
2023-02-11 18:36:04 +0100

    receiveMethod removed

--- ClientPlutoManager.cs
+++ ClientPlutoManager.cs
@@ -16,38 +16,6 @@ namespace Plutonication
             Port = port;
         }
 
-        public Method ReceiveMethod()
-        {
-            return ReceiveMethodAsync().GetAwaiter().GetResult();
-        }
-        public async Task<Method> ReceiveMethodAsync()
-        {
-            const int NUM_OF_BYTES_REQUIRED = 3;
-            NetworkStream stream = Client.GetStream();
-            Byte[] data = new Byte[256];
-            Int32 bytes = await stream.ReadAsync(data, 0, data.Length);
-            if (data[0] != (byte)MessageCode.Method)
-            {
-                throw new Exception(String.Format("{0} not {1} of value {2}. Current value: {3}",
-                    nameof(MessageCode), nameof(MessageCode.Method), ((int)MessageCode.Method), data[0]));
-            }
-            if (bytes > NUM_OF_BYTES_REQUIRED)
-            {
-                return new Method(data[1], data[2], data.Skip(3).ToArray());
-            }
-            else if (bytes == NUM_OF_BYTES_REQUIRED)
-            {
-                // No parrameters of Method => pass empty array
-                return new Method(data[1], data[2], new Byte[0]);
-            }
-            else
-            {
-                throw new Exception(String.Format(
-                    "Not enought bytes received to correspond Method format. Bytes Received: {0}, num of bytes requered: {1}",
-                 bytes, NUM_OF_BYTES_REQUIRED));
-            }
-        }
-
         public void Connect()
         {
             int port = 8080;
