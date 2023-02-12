b8bface5c1a101f639dee12baf706f580a495460
cisar2218 <cisar2218@gmail.com>
2023-02-12 09:26:45 +0100

    async method revented because they have to be tested first

--- PlutoManager.cs
+++ PlutoManager.cs
@@ -17,10 +17,23 @@ namespace Plutonication
         
         public PlutoMessage ReceiveMessage()
         {
-            return ReceiveMessageAsync().GetAwaiter().GetResult();
+            NetworkStream stream = Client.GetStream();
+            Byte[] data = new Byte[256];
+
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            if (!(bytes > 0))
+            {
+                throw new Exception("No data received.");
+            }
+            int customDataLenght = bytes-1;
+            Byte[] customData = new Byte[customDataLenght]; 
+            Array.Copy(data, 1, customData, 0, customDataLenght);
+            return new PlutoMessage((MessageCode)data[0], customData);
         }
         public async Task<PlutoMessage> ReceiveMessageAsync()
         {
+            throw new NotImplementedException();
+            // TODO implement and test properly
             NetworkStream stream = Client.GetStream();
             Byte[] data = new Byte[256];
 
@@ -37,19 +50,25 @@ namespace Plutonication
 
         public void SendMessage(PlutoMessage message)
         {
-            SendMessageAsync(message).GetAwaiter().GetResult();
+            NetworkStream stream = Client.GetStream();
+            byte[] msg = message.ToByteArray();
+            stream.Write(msg, 0, msg.Length);
         }
 
         public void SendMessage(MessageCode code)
         {
-            SendMessageAsync(code).GetAwaiter().GetResult();
+            SendMessage(new PlutoMessage(code, String.Empty));
         }
         public async Task SendMessageAsync(MessageCode code)
         {
+            throw new NotImplementedException();
+            // TODO implement and test properly
             await SendMessageAsync(new PlutoMessage(code, String.Empty));
         }
          public async Task SendMessageAsync(PlutoMessage message)
         {
+            throw new NotImplementedException();
+            // TODO implement and test properly
             NetworkStream stream = Client.GetStream();
             byte[] msg = message.ToByteArray();
             await stream.WriteAsync(msg, 0, msg.Length);
@@ -57,6 +76,8 @@ namespace Plutonication
 
         public async Task SendMethodAsync(Method transaction)
         {
+            throw new NotImplementedException();
+            // TODO implement and test properly
             Byte[] msg = new Byte[transaction.Parameters.Length + 2];
 
             msg[0] = transaction.ModuleIndex;
@@ -67,7 +88,13 @@ namespace Plutonication
         }
         public void SendMethod(Method transaction)
         {
-            SendMethodAsync(transaction).GetAwaiter().GetResult();
+            Byte[] msg = new Byte[transaction.Parameters.Length + 2];
+
+            msg[0] = transaction.ModuleIndex;
+            msg[1] = transaction.CallIndex;
+            transaction.Parameters.CopyTo(msg, 2);
+
+            SendMessage(new PlutoMessage(MessageCode.Method, msg));
         }
 
         public static IPAddress GetMyIpAddress()
