389af239b0c031a067789af22720454065750458
rostislavlitovkin <rostislavlitovkin@gmail.com>
2023-06-27 17:02:26 +0200

    New plutonication

---
+++ PlutonicationMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Plutonication
+{
+    public class PlutonicationMessage
+    {
+        public int Room { get; set; }
+        public string Data { get; set; } = "";
+    }
+}
+
---
+++ PlutonicationWalletClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Sockets;
+using SocketIOClient;
+
+namespace Plutonication
+{
+	public class PlutonicationWalletClient
+	{
+		public static SocketIO? Client;
+
+		public static async Task InitializeAsync(AccessCredentials c, string pubkey)
+		{
+			Client = new SocketIO(c.Url);
+
+            await Client.ConnectAsync();
+
+			await SendPublicKeyAsync(pubkey);
+        }
+
+		public static async Task SendPublicKeyAsync(string pubkey)
+		{
+            await Client.EmitAsync(
+                "pubkey",
+                new PlutonicationMessage { Data = pubkey });
+        }
+
+        public static async Task DisconnectAsync()
+        {
+            await Client.DisconnectAsync();
+            Client.Dispose();
+        }
+    }
+}
+
