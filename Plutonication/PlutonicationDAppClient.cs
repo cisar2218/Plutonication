80b323841881bc71e336b85361eaa9cfe6c71f76
rostislavlitovkin <rostislavlitovkin@gmail.com>
2024-03-21 19:43:50 +0100

    `SocketIOOptions` added to `DAppClient.InitializeAsync`

--- Plutonication/PlutonicationDAppClient.cs
+++ Plutonication/PlutonicationDAppClient.cs
@@ -17,8 +17,30 @@ namespace Plutonication
         public static async Task<PlutonicationAccount> InitializeAsync(
             AccessCredentials ac,
             Action<string> onReceivePublicKey
+        )
+        {
+            return await InitializeAsync(
+                ac,
+                onReceivePublicKey,
+                new SocketIOOptions()
+            );
+        }
+
+        /// <summary>
+        /// Initializes the Plutonication DApp client connection.
+        /// </summary>
+        /// <param name="ac">The credentials required for connecting to the Plutonication server.</param>
+        /// <param name="onReceivePublicKey">Callback function to handle the received public key.</param>
+        /// <param name="socketIOOptions">Extra configuration for the WebSocketClient</param>
+        /// <returns>PlutonicationAccount ~ external wallet account.</returns>
+        /// <exception cref="PlutonicationConnectionException">Error when unable to establish connection with the websocket server provided in the access credentials.</exception>
+        /// <exception cref="WrongMessageReceivedException">Error when receiving a wrong message from the Plutonication server.</exception>
+        public static async Task<PlutonicationAccount> InitializeAsync(
+            AccessCredentials ac,
+            Action<string> onReceivePublicKey,
+            SocketIOOptions socketIOOptions
         ) {
-            var client = new SocketIO(ac.Url);
+            var client = new SocketIO(ac.Url, socketIOOptions);
 
             // Wait for the dApp socket client to connect.
             try
